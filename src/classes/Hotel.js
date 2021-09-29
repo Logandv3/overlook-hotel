be6a846c17e99fdeb1385ef9eb9faf80add59ccb
Logan Vincent <ldvsimp@gmail.com>
2021-09-29 15:29:32 -0600

    Add code to reset roomAvailability in Hotel

--- src/classes/Hotel.js
+++ src/classes/Hotel.js
@@ -4,6 +4,8 @@ class Hotel {
     this.roomAvailability = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25];
   };
   filterAvailableRooms(date) {
+    this.roomAvailability = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25];
+
     let unavailableRooms = this.allBookings.reduce((arr, room) => {
       if (room.date === date) {
         arr.push(room.roomNumber)
