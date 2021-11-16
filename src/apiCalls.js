6f3a3e1396fbc6ecc3c56f5c9bdab95e68135e73
Logan Vincent <ldvsimp@gmail.com>
2021-11-16 15:17:34 -0700

    Add new fetch path to include API

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -3,31 +3,31 @@ import { updateData } from './scripts.js';
 // import Bookings from './src/classes/Bookings.js';
 
 export const allCustomersPromise = () => {
-  return fetch('http://localhost:3001/api/v1/customers')
+  return fetch('https://overlookdata.herokuapp.com/api/v1/customers')
   .then(response => response.json())
 };
 
 
 export const singleCustomerPromise = (customerUsername) => {
-  return fetch(`http://localhost:3001/api/v1/customers/${customerUsername}`)
+  return fetch(`https://overlookdata.herokuapp.com/api/v1/customers/${customerUsername}`)
   .then(response => response.json())
 };
 
 
 export const roomPromise = () => {
-  return fetch('http://localhost:3001/api/v1/rooms')
+  return fetch('https://overlookdata.herokuapp.com/api/v1/rooms')
   .then(response => response.json())
 };
 
 
 export const bookingsPromise = () => {
-  return fetch('http://localhost:3001/api/v1/bookings')
+  return fetch('https://overlookdata.herokuapp.com/api/v1/bookings')
   .then(response => response.json())
 };
 
 
 export const bookUserStay = (newStay, event) => {
-  fetch('http://localhost:3001/api/v1/bookings', {
+  fetch('https://overlookdata.herokuapp.com/api/v1/bookings', {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify(newStay),
